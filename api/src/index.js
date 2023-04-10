5a347e20398288183a252830284ae5f70f5010fa
Javier Mejia <admin@javierm.net>
2023-04-10 04:42:58 -0500

    add cluster workers to the API

--- api/src/index.js
+++ api/src/index.js
@@ -1,16 +1,28 @@
+import cluster from 'node:cluster'
+import { availableParallelism } from 'node:os'
+
 import server from './server.js'
 import db from './services/db.js'
 import config from '../config.js'
 
-db.connect()
-db.instance.once('open', async () => {
-	console.log('Connected to MongoDB')
-	server.emit('ready')
-})
-
-server.on('ready', () => server.listen(
-	config.apiPort,
-	() => console.log(`API Server started on port ${config.apiPort}`)
-))
-
-server.on('error', error => console.error(error))
+if (cluster.isPrimary) {
+	console.log(`[versa-api] primary ${process.pid} is running`)
+	
+	const availableCPUs = availableParallelism()
+	
+	for (let i = 0; i < availableCPUs; i++) {
+		cluster.fork()
+	}
+	
+	cluster.on('exit', (worker, code) => {
+		console.log(`[versa-api] worker ${worker.process.pid} died with code ${code}`)
+	})
+} else {
+	db.connect()
+	db.instance.once('open', async () => {
+		server.emit('ready')
+	})
+	
+	server.on('ready', () => server.listen(config.apiPort))
+	server.on('error', error => console.error(error))
+}
