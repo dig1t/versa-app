2a4343a8fcb4c49145e18c3de318f7fbe1cfc5cb
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2023-05-11 07:54:26 -0500

    add twemoji support

--- app/src/containers/Emojify.js
+++ app/src/containers/Emojify.js
@@ -1,19 +1,48 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { Children } from 'react'
+import PropTypes from 'prop-types'
 import twemoji from 'twemoji'
 
 const options = {
 	ext: '.svg',
-	folder: 'svg',
-	base: 'twemoji/assets'
+	folder: '/svg',
+	base: 'assets/i/emoji',
+	className: 'emoji'
 }
 
 const Emojify = ({ children }) => {
+	const childrenRefs = useRef([])
+	
+	useEffect(() => {
+		const parseTwemoji = () => {
+			Object.values(childrenRefs.current).forEach((ref) => {
+				const node = ref.current
+				
+				twemoji.parse(node, options)
+			})
+		}
+		
+		parseTwemoji()
+	}, [])
+	
 	return <>
-		{Children.map(children, (child) => {
-			return twemoji.parse(child, options)
+		{Children.map(children, (child, index) => {
+			if (typeof child === 'string') {
+				console.warn(`Twemoji can't parse string child when noWrapper is set. Skipping child "${child}"`)
+				return child
+			}
+			
+			childrenRefs.current[index] = childrenRefs.current[index] || useRef(null)
+			
+			return React.cloneElement(child, { ref: childrenRefs.current[index] })
 		})}
 	</>
+}
+
+Emojify.propTypes = {
+	children: PropTypes.node,
+	options: PropTypes.object,
+	tag: PropTypes.string
 }
 
 export default Emojify
