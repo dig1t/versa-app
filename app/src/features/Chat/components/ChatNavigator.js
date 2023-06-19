db8f1d652d98391410186598181701055b6bd44c
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2023-06-19 18:02:54 -0500

    update

---
+++ app/src/features/Chat/components/ChatNavigator.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ChatNavigator = () => {
+	return <div className="navigator">
+    <div className="wrap grid-g">
+      <div className="col-2 col-desktop-3">
+        <div className="box">
+          <div className="box-header">
+            <h1>Chat</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+	</div>
+}
+
+export default ChatNavigator
