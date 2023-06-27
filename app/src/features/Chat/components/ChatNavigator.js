6f185a67b6136af0c03f109430f22e509d2a5802
Javier Mejia <4343867+dig1t@users.noreply.github.com>
2023-06-26 20:05:43 -0500

    prog

--- app/src/features/Chat/components/ChatNavigator.js
+++ app/src/features/Chat/components/ChatNavigator.js
@@ -4,10 +4,8 @@ const ChatNavigator = () => {
 	return <div className="navigator">
     <div className="wrap grid-g">
       <div className="col-2 col-desktop-3">
-        <div className="box">
-          <div className="box-header">
-            <h1>Chat</h1>
-          </div>
+        <div className="box-header">
+          <h1>Chat</h1>
         </div>
       </div>
     </div>
--- app/src/features/Chat/components/ChatRoute.js
+++ app/src/features/Chat/components/ChatRoute.js
@@ -2,15 +2,16 @@ import React from 'react'
 
 import Layout from '../../Core/components/Layout.js'
 import useProfile from '../../User/hooks/useProfile.js'
+import ChatNavigator from './ChatNavigator.js'
 
 const ChatRoute = () => {
 	const profile = useProfile(true)
 	
-	return <Layout page="content">
+	return <Layout page="chat" fullWidth>
 		<div className="wrap grid-g">
 			<div className="col-12 col-desktop-8">
 				<div className="box">
-					
+					<ChatNavigator />
 				</div>
 			</div>
 		</div>
